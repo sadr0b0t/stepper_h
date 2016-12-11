ef52aad3211273fde36a98edfe2510164538b57c
1i7 <ii1i7@yandex.ru>
2016-12-11 23:10:33 +0300

    Настройка для максимального количества моторов

--- stepper_h/stepper_lib_config.h
+++ stepper_h/stepper_lib_config.h
@@ -4,6 +4,10 @@
 // Настройки библиотеки на этапе компиляции
 // Compile time library config
 
+// максимальное количество шаговых моторов
+// maximun number of stepper motors
+#define MAX_STEPPERS 6
+
 // включить отладку через последовательный порт
 // enable serial port debug messages
 //#define DEBUG_SERIAL
